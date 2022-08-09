9d264acd7bb03834512bf97bc37a2e110eb910b2
diogothug <diogogenuino@gmail.com>
2022-08-09 10:43:25 -0300

    Adicionado js

---
+++ script.js
@@ -0,0 +1,4 @@
+alert("ola")
+let entrada_nova_tarefa = document.querySelector('#entrada_nova_tarefa');
+let botao_adicionar = document.querySelector('#botao_adicionar');
+let lista_tarefas = document.querySelector('#lista_tarefas');
